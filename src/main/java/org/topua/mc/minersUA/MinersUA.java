dc8e83a133660e554dd460a91e63f0929430c22d
Demoused <demousedhub@gmail.com>
2024-07-30 18:19:04 +0300

    Створення плагину

---
+++ src/main/java/org/topua/mc/minersUA/MinersUA.java
@@ -0,0 +1,17 @@
+package org.topua.mc.minersUA;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinersUA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
